199bbf11821dfda5ef33f06ca3e7c07941f96e6a
soufiane-dahani <dahanissoufiane@gmail.com>
2025-09-01 15:35:57 +0100

    add header

---
+++ module_05/ex01/Bureaucrat.cpp
@@ -0,0 +1,55 @@
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat(const std::string& name, int grade) : _name(name)
+{
+    if (grade < 1)
+        throw GradeTooHighException();
+    if (grade > 150)
+        throw GradeTooLowException();
+    _grade = grade;
+};
+
+Bureaucrat::~Bureaucrat()
+{
+    std::cout << "default destructor " << std::endl ;
+}
+
+const std::string&  Bureaucrat::getName() const
+{
+    return _name ;
+}
+
+int  Bureaucrat::getGrade() const
+{
+    return _grade ;
+}
+
+void Bureaucrat::incrementGrade()
+{
+    if (_grade <= 1)
+        throw GradeTooHighException();
+    _grade--;
+}
+
+void Bureaucrat::decrementGrade()
+{
+    if (_grade >= 150)
+        throw GradeTooLowException();
+    _grade++;
+}
+
+const char* Bureaucrat::GradeTooHighException::what() const throw() {
+    return "Grade is too high!";
+}
+
+const char* Bureaucrat::GradeTooLowException::what() const throw() {
+    return "Grade is too low!";
+}
+
+
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& b)
+{
+    os << b.getName() << ", bureaucrat grade " << b.getGrade() << std::endl;
+    return os;
+}
+
---
+++ module_05/ex01/main.cpp
@@ -0,0 +1,18 @@
+#include "Bureaucrat.hpp"
+#include <iostream>
+
+int main()
+{
+    try {
+        std::cout << "try risky code \n";
+        Bureaucrat b("dahani", 1);
+        std::cout << b ;
+        b.decrementGrade();
+        std::cout << b ;
+    }
+    catch (const std::exception &e)
+    {
+        std::cout << "something happen error " << e.what() << std::endl ;
+    }
+    return 0;
+}
