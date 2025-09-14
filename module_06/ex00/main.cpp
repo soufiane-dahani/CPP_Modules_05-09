0edb2fdfe07dab71b59a2cda9fefecdb0237d37d
soufiane-dahani <dahanissoufiane@gmail.com>
2025-09-14 15:36:52 +0100

    edit code

--- module_06/ex00/main.cpp
+++ module_06/ex00/main.cpp
@@ -1,42 +1,13 @@
 #include <iostream>
 #include "ScalarConverter.hpp"
 
-int main()
+int main(int ac , char **av)
 {
-    std::cout << "==== Test with int ====" << std::endl;
-    ScalarConverter::convert("42");
-    ScalarConverter::convert("-123");
-
-    std::cout << "\n==== Test with char ====" << std::endl;
-    ScalarConverter::convert("a");
-    ScalarConverter::convert("Z");
-
-    std::cout << "\n==== Test with float ====" << std::endl;
-    ScalarConverter::convert("42.0f");
-    ScalarConverter::convert("-4.2f");
-    ScalarConverter::convert("0.0f");
-
-    std::cout << "\n==== Test with double ====" << std::endl;
-    ScalarConverter::convert("42.0");
-    ScalarConverter::convert("-4.2");
-    ScalarConverter::convert("0.0");
-
-    std::cout << "\n==== Test with special literals ====" << std::endl;
-    ScalarConverter::convert("nan");
-    ScalarConverter::convert("nanf");
-    ScalarConverter::convert("+inf");
-    ScalarConverter::convert("+inff");
-    ScalarConverter::convert("-inf");
-    ScalarConverter::convert("-inff");
-
-    std::cout << "\n==== Test with edge cases ====" << std::endl;
-    ScalarConverter::convert("2147483647");
-    ScalarConverter::convert("-2147483648");
-    ScalarConverter::convert("340282346638528859811704183484516925440.0f");
-
-    std::cout << "\n==== Test Errors ====" << std::endl;
-    ScalarConverter::convert("hello");
-    ScalarConverter::convert("ggg");
-
+    if (ac == 2)
+    {
+        ScalarConverter::convert(av[1]);
+    }
+    else
+        std::cout << "Error" << std::endl;
     return 0;
 }
