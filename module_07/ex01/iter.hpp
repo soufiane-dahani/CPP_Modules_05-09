41f82823a2c58543122e750dba1dfdf2a92ef10d
soufiane-dahani <dahanissoufiane@gmail.com>
2025-10-09 17:45:00 +0100

    const test

--- module_07/ex01/iter.hpp
+++ module_07/ex01/iter.hpp
@@ -3,17 +3,34 @@
 
 template <typename T>
 
-void iter(T *array, size_t length, void (*func)(T const &))
+void iter(const T *array, size_t length, void (*func)(const T &))
 {
     if (!array || !func)
-        return;
+    return;
     for (size_t i = 0; i < length; i++)
-        func(array[i]);
+    func(array[i]);
 }
 
 template <typename T>
 
-void printElement(T const &x)
+void iter(T *array, size_t length, void (*func)(T &))
+{
+    if (!array || !func)
+    return;
+    for (size_t i = 0; i < length; i++)
+    func(array[i]);
+}
+
+template <typename T>
+
+void printElement(const T  &x)
+{
+    std::cout << x << std::endl;
+}
+
+template <typename T>
+
+void print1337(T &x)
 {
     std::cout << x << std::endl;
 }
--- module_07/ex01/main.cpp
+++ module_07/ex01/main.cpp
@@ -6,8 +6,11 @@
 int main(void)
 {
     int intArray[5] = {1, 2, 3, 4, 5};
+    const int intArray2[5] = {1, 2, 3, 4, 5};
     std::cout << "Integer array:" << std::endl;
-    iter(intArray, 5, printElement);
+    iter(intArray2, 5, printElement);
+    std::cout << "Integer array:" << std::endl;
+    iter(intArray, 5, print1337);
     std::cout << std::endl;
     return 0;
 }
