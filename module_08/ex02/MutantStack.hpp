a899730bd7676afdcda1552f8398ce3f650f80ef
kali <kali@kali>
2025-10-20 12:31:25 +0100

    understand problem and try to fix it

--- module_08/ex02/MutantStack.hpp
+++ module_08/ex02/MutantStack.hpp
@@ -1,2 +1,22 @@
 #pragma once 
+#include <stack>
+#include <deque>
+template <typename T, typename Container = std::deque<T> >
+
+class MutantStack : public std::stack<T, Container>
+{
+    public:
+        MutantStack();
+        MutantStack(const MutantStack& other);
+        MutantStack& operator=(const MutantStack& other);
+        ~MutantStack();
+
+        typedef typename Container::iterator iterator;
+        typedef typename Container::const_iterator const_iterator;
+
+        iterator begin();
+        iterator end();
+        const_iterator begin() const;
+        const_iterator end() const; 
+};
 
